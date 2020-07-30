976085dfa1fa0456fb77091031f4c979eb1ee0e8
Matthew Eric Bassett <matthew@emurgo.io>
2020-07-30 17:51:43 +0100

    mod - sending signedTxs to yoroi/iohk backend.
    also some changes for more useful error messages.

--- src/services/signedTransaction.ts
+++ src/services/signedTransaction.ts
@@ -1,42 +1,35 @@
 import axios from "axios";
 import { Request, Response } from "express";
 
-import { contentTypeHeaders } from "../utils";
 
 import { SignedTransaction } from "cardano-wallet";
 
-const submissionEndpoint = "https://adalite.io/api/txs/submit";
+const submissionEndpoint = "https://backend.yoroiwallet.com/api/submit/tx";
 
-interface SubmissionData {
-    txHash: string;
-    txBody: string;
+const contentTypeHeaders = {"Content-Type": "application/cbor"};
 
-}
 
 export const handleSignedTx = async (req: Request, res: Response):Promise<void>=> { 
   if(!req.body.signedTx)
     throw new Error ("No signedTx in body");
 
   const buffer = Buffer.from(req.body.signedTx, "base64");
-  const txHash = (() => {
-    const tx = SignedTransaction.from_bytes(buffer);
-    const txHash = tx.id();
-    tx.free();
-    return txHash;
-  })();
-
-  const submission : SubmissionData = { txHash: txHash
-    , txBody: buffer.toString("hex")
-  };
-  const endpointResponse = await axios.post(submissionEndpoint, JSON.stringify(submission), contentTypeHeaders);
-
-  if(endpointResponse.status === 200){
-    if(endpointResponse.data.Left)
-      throw Error(`Error tyrying to send transaction: ${endpointResponse.data.Left}`);
-    res.send([]);
-    return;
+  try {
+    const endpointResponse = await axios({ method:"post"
+      , url: submissionEndpoint
+      , data: buffer
+      , headers: contentTypeHeaders}); 
+    if(endpointResponse.status === 202){
+      if(endpointResponse.data.Left){
+        const msg = `Transaction was rejected: ${endpointResponse.data.Left}`;
+        throw Error(msg);
+      }
+      res.send([]);
+      return;
+    }
+  } catch(error) {
+    const msg = `Error trying to send transaction: ${error} - ${error.response.data}`;
+    throw Error(msg);
   }
-  
-  throw new Error(`Error trying to send transaction: ${endpointResponse.data}`);
 
 };
