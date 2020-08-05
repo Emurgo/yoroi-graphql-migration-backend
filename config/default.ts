7947106a76e193be8c820e5428248a8ad5ed04d4
Matthew Eric Bassett <matthew@emurgo.io>
2020-08-05 16:11:52 +0100

    add - config options for signedtx endpoint, et al.

--- config/default.ts
+++ config/default.ts
@@ -9,6 +9,8 @@ export default {
     addressRequestLimit: 50,
     apiResponseLimit: 50,
     graphqlEndpoint: 'http://localhost:3100/',
+    txSubmissionEndpoint: 'https://backend.yoroiwallet.com/api/submit/tx',
+    smashEndpoint: 'https://stage-smash.yoroiwallet.com/api/v1/metadata/',
     port: 8082,
     txsHashesRequestLimit: 150
   }
