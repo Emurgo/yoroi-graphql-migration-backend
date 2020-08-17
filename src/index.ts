f33a65e748e3071dfc9f4d291f5decc8c419e279
Marek Mahut <marek.mahut@gmail.com>
2020-08-17 19:55:14 +0200

    index: adding current time to /status endpoint (#37)
    (cherry picked from commit 477aba1af4845ceb9a70875db253b23307940056)

--- src/index.ts
+++ src/index.ts
@@ -282,7 +282,7 @@ const getStatus = async (req: Request, res:  Response) => {
       }
     }
   }
-  res.send({ isServerOk: true, isMaintenance: false }); 
+  res.send({ isServerOk: true, isMaintenance: false, serverTime: Date.now() });
 };
 
 const routes : Route[] = [ { path: "/v2/bestblock"
