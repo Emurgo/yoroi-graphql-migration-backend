afa4c94a32bae19168bda9a59adfa7f081233f50
Pedro <pedromtcosta@users.noreply.github.com>
2021-10-26 16:40:03 -0300

    Exclude UTxOs spent in the reference block as well

--- src/Transactions/utxoFunctions.ts
+++ src/Transactions/utxoFunctions.ts
@@ -22,7 +22,7 @@ BEGIN
               INNER JOIN tx ON tx_in.tx_in_id = tx.id
               INNER JOIN block ON tx.block_id = block.id
               LEFT JOIN tx input_tx ON tx_in.tx_in_id = input_tx.id
-          WHERE block.block_no < _reference_block_no
+          WHERE block.block_no <= _reference_block_no
               AND input_tx.valid_contract
               AND tx_in.tx_out_id = _tx_out_tx_id
               AND tx_in.tx_out_index::smallint = _tx_out_index
@@ -53,7 +53,7 @@ BEGIN
               INNER JOIN tx ON collateral_tx_in.tx_in_id = tx.id
               INNER JOIN block ON tx.block_id = block.id
               LEFT JOIN tx collateral_tx ON collateral_tx_in.tx_in_id = collateral_tx.id
-          WHERE block.block_no < _reference_block_no
+          WHERE block.block_no <= _reference_block_no
               AND collateral_tx.valid_contract = false
               AND collateral_tx_in.tx_out_id = _tx_out_tx_id
               AND collateral_tx_in.tx_out_index::smallint = _tx_out_index
