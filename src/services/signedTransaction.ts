d6d9940dc37a9fc9d6df969b841c9b8a0e414ce1
Pedro <pedromtcosta@users.noreply.github.com>
2021-10-28 15:44:28 -0300

    Send TX ID on message to the queue

--- src/services/signedTransaction.ts
+++ src/services/signedTransaction.ts
@@ -1,6 +1,7 @@
 import config from "config";
 import axios from "axios";
 import { Request, Response } from "express";
+import { calculateTxId } from "../utils";
 
 const submissionEndpoint: string = config.get("server.txSubmissionEndpoint");
 
@@ -16,13 +17,15 @@ export const handleSignedTx = async (
 ): Promise<void> => {
   if (!req.body.signedTx) throw new Error("No signedTx in body");
 
-  const buffer = Buffer.from(req.body.signedTx, "base64");
-
   try {
+    const buffer = Buffer.from(req.body.signedTx, "base64");
+    const txId = await calculateTxId(buffer.toString("base64"));
+
     await axios({
       method: "post"
       , url: submissionEndpoint
       , data: {
+        txId: txId,
         signedTx: buffer.toString("base64")
       }
       , headers: contentTypeHeaders
