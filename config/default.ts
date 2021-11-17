54d3ef20392e417ee350ad4fafd5a82ac01b6b9f
Bart <bartlomiejrybaa@gmail.com>
2021-11-17 09:20:52 +0100

    included Milkomeda configs

--- config/default.ts
+++ config/default.ts
@@ -24,5 +24,19 @@ export default {
     port: process.env.PORT || 8082,
     txsHashesRequestLimit: 150,
   },
-  blockfrostProjectKey: process.env.BLOCKFROST || ""
+  blockfrostProjectKey: process.env.BLOCKFROST || "",
+  milkomeda: {
+    minValue: 10000000,
+    address: "addr_test1qq6gwl46frfwfk593pqjg39vym476s64n9cmykdl7lv7w32jdqlum6v4sc3l3w9nmuf3ean86n2y7pl83tpxl2qjdw8qmjljz2",
+    tokens: [
+      {
+        name: "ADA",
+        policyId: "?"
+      },
+      {
+        name: "TADA",
+        policyId: "?"
+      },
+    ]
+  }
 };
