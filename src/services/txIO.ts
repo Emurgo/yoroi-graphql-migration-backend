2064efac55d383e527e2f7ee35af0f0895d382ca
Özgür Akkurt <oezgurmakkurt@gmail.com>
2021-11-29 10:31:34 +0300

    parallelize txs/io queries

--- src/services/txIO.ts
+++ src/services/txIO.ts
@@ -10,9 +10,11 @@ export const handleGetTxIO = (pool: Pool) => async (req: Request, res:Response):
 
   if (!(await txExists(pool, tx_hash))) throw new Error("transaction can't be found");
 
-  const inputs = await getInputs(pool, tx_hash);
-  const collateralInputs = await getCollateralInputs(pool, tx_hash);
-  const outputs = await getOutputs(pool, tx_hash);
+  const [inputs, collateralInputs, outputs] = await Promise.all([
+    getInputs(pool, tx_hash),
+    getCollateralInputs(pool, tx_hash),
+    getOutputs(pool, tx_hash)
+  ]);
 
   res.send({
     inputs, collateralInputs, outputs
