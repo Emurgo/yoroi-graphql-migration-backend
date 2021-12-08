d902ce8fcf603adad0993947418a145e9bbe204d
Özgür Akkurt <oezgurmakkurt@gmail.com>
2021-12-08 17:37:17 +0300

    modify txIO endpoint so it uses one query

--- src/services/txIO.ts
+++ src/services/txIO.ts
@@ -8,89 +8,46 @@ export const handleGetTxIO = (pool: Pool) => async (req: Request, res:Response):
 
   const tx_hash = req.params.tx_hash.trim();
 
-  const tx_id = await getTxId(pool, tx_hash);
-
-  if (tx_id === null) throw new Error("transaction can't be found");
-
-  const [inputs, collateralInputs, outputs] = await Promise.all([
-    getInputs(pool, tx_hash),
-    getCollateralInputs(pool, tx_hash),
-    getOutputs(pool, tx_id, tx_hash)
-  ]);
-
-  res.send({
-    inputs, collateralInputs, outputs
-  });
-};
-
-const getTxId = async (pool: Pool, tx_hash: string): Promise<any> => {
-  const query = `
-    select tx.id from tx
-    where encode(tx.hash, 'hex') = $1
-    limit 1`;
-
   const result = await pool.query(query, [tx_hash]);
 
   if (result.rowCount !== 1) {
-    return null;
+    throw new Error("transaction can't be found");
   }
 
-  return result.rows[0].id;
+  const inputs = result.rows[0].in_addr_val_pairs || [];
+  const collateralInputs = result.rows[0].collateral_in_addr_val_pairs || [];
+  const outputs = result.rows[0].out_addr_val_pairs || [];
+
+  res.send({
+    inputs: inputs.map((obj: any): TransInputFrag => ({
+      address: obj.f1,
+      amount: obj.f2.toString(),
+      id: obj.f3.concat(obj.f4.toString()),
+      index: obj.f4,
+      txHash: obj.f3,
+      assets: extractAssets(obj.f5)
+    })),
+    collateralInputs: collateralInputs.map((obj: any): TransInputFrag => ({
+      address: obj.f1,
+      amount: obj.f2.toString(),
+      id: obj.f3.concat(obj.f4.toString()),
+      index: obj.f4,
+      txHash: obj.f3,
+      assets: extractAssets(obj.f5)
+    })),
+    outputs: outputs.map((obj: any): TransOutputFrag => ({
+      address: obj.f1,
+      amount: obj.f2.toString(),
+      assets: extractAssets(obj.f3)
+    }))
+  });
 };
 
-const getInputs = async (pool: Pool, tx_hash: string): Promise<Array<TransInputFrag>> => {
-  const query = `
-    select * from in_addr_val_pairs(decode($1, 'hex'));`;
-
-  const result = await pool.query(query, [tx_hash]);
-
-  if (result.rows[0].in_addr_val_pairs === null) {
-    return [];
-  }
-
-  return result.rows[0].in_addr_val_pairs.map((obj: any): TransInputFrag => ({
-    address: obj.f1,
-    amount: obj.f2.toString(),
-    id: obj.f3.concat(obj.f4.toString()),
-    index: obj.f4,
-    txHash: obj.f3,
-    assets: extractAssets(obj.f5)
-  }));
-};
-
-const getCollateralInputs = async (pool: Pool, tx_hash: string): Promise<Array<TransInputFrag>> => {
-  const query = `
-    select * from collateral_in_addr_val_pairs(decode($1, 'hex'));`;
-
-  const result = await pool.query(query, [tx_hash]);
-
-  if (result.rows[0].collateral_in_addr_val_pairs === null) {
-    return [];
-  }
-
-  return result.rows[0].collateral_in_addr_val_pairs.map((obj: any): TransInputFrag => ({
-    address: obj.f1,
-    amount: obj.f2.toString(),
-    id: obj.f3.concat(obj.f4.toString()),
-    index: obj.f4,
-    txHash: obj.f3,
-    assets: extractAssets(obj.f5)
-  }));
-};
-
-const getOutputs = async (pool: Pool, tx_id: number, tx_hash: string): Promise<Array<TransOutputFrag>> => {
-  const query = `
-    select * from out_addr_val_pairs($1, decode($2, 'hex'));`;
-
-  const result = await pool.query(query, [tx_id, tx_hash]);
-
-  if (result.rows[0].out_addr_val_pairs === null) {
-    return [];
-  }
-
-  return result.rows[0].out_addr_val_pairs.map((obj: any): TransOutputFrag => ({
-    address: obj.f1,
-    amount: obj.f2.toString(),
-    assets: extractAssets(obj.f3)
-  }));
-};
+const query = `
+  select 
+    in_addr_val_pairs(tx.hash),
+    collateral_in_addr_val_pairs(tx.hash),
+    out_addr_val_pairs(tx.id, tx.hash)
+  from tx
+  where encode(tx.hash, 'hex') = $1
+  limit 1;`;
