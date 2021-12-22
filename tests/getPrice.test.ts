aab87b2c00a4f76bf438852c6e2b1367b7dde76e
Jakub Smahovsky <jakub.smahovsky@vacuumlabs.com>
2021-12-22 22:35:05 +0100

    step2: price history endpoint

--- tests/getPrice.test.ts
+++ tests/getPrice.test.ts
@@ -8,6 +8,8 @@ const testableUri = endpoint + "getPrice";
 const tickerADA = "ADA";
 const tickerInvalid = "potato";
 const unitOfAccount = "USD";
+const hourlySample = 100;
+const dailySample = 1000;
 
 describe("/getPrice", function () {
   it("should get ADA price", async () => {
@@ -16,12 +18,46 @@ describe("/getPrice", function () {
       to: unitOfAccount,
     });
     expect(result.data).to.have.property(tickerADA);
-    expect(result.data[tickerADA]).to.have.property("PRICE");
-    expect(result.data[tickerADA]).to.have.property("LASTUPDATE");
-    expect(result.data[tickerADA]).to.have.property("CHANGEPCT24HOUR");
-    expect(result.data[tickerADA].PRICE).to.be.greaterThan(0);
-    expect(result.data[tickerADA].LASTUPDATE).to.be.greaterThan(0);
-    expect(result.data[tickerADA].CHANGEPCT24HOUR).not.to.be.eq(0);
+    const currentAda = result.data[tickerADA];
+    expect(currentAda).to.have.property("PRICE");
+    expect(currentAda).to.have.property("LASTUPDATE");
+    expect(currentAda).to.have.property("CHANGEPCT24HOUR");
+    expect(currentAda.PRICE).to.be.greaterThan(0);
+    expect(currentAda.LASTUPDATE).to.be.greaterThan(0);
+    expect(currentAda.CHANGEPCT24HOUR).not.to.be.eq(0);
+  });
+
+  it("should get ADA history", async () => {
+    const result = await axios.post(testableUri, {
+      from: [tickerADA],
+      to: unitOfAccount,
+    });
+    expect(result.data).to.have.property(tickerADA);
+    expect(result.data[tickerADA]).to.have.property("historyHourly");
+    const hourlyAda = result.data[tickerADA].historyHourly;
+    expect(hourlyAda).to.have.length.greaterThan(hourlySample);
+    expect(hourlyAda[hourlySample]).to.have.property("time");
+    const dateHour = new Date();
+    dateHour.setUTCMinutes(0, 0, 0);
+    const timestampHour =
+      dateHour.getTime() / 1000 -
+      (hourlyAda.length - 1 - hourlySample) * 60 * 60;
+    expect(hourlyAda[hourlySample].time).to.be.eq(timestampHour);
+    expect(hourlyAda[hourlySample]).to.have.property("price");
+    expect(hourlyAda[hourlySample].price).to.be.greaterThan(0);
+
+    expect(result.data[tickerADA]).to.have.property("historyDaily");
+    const dailyAda = result.data[tickerADA].historyDaily;
+    expect(dailyAda).to.have.length.greaterThan(dailySample);
+    expect(dailyAda[dailySample]).to.have.property("time");
+    const dateDay = new Date();
+    dateDay.setUTCHours(0, 0, 0, 0);
+    const timestampDay =
+      dateDay.getTime() / 1000 -
+      (dailyAda.length - 1 - dailySample) * 24 * 60 * 60;
+    expect(dailyAda[dailySample].time).to.be.eq(timestampDay);
+    expect(dailyAda[dailySample]).to.have.property("price");
+    expect(dailyAda[dailySample].price).to.be.greaterThan(0);
   });
 
   it("should not crash on invalid token", async () => {
