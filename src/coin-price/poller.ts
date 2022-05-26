fef0e4023c5712be8c0959460b10623c29c85673
yushi <yushihuang@gmail.com>
2022-05-26 23:20:02 +0800

    add comment

--- src/coin-price/poller.ts
+++ src/coin-price/poller.ts
@@ -108,6 +108,7 @@ async function getTickersFromS3Since(
 }
 
 export async function start() {
+  // do nothing if there isn't a flag file present in the S3 bucket
   try {
     await util.promisify(S3.getObject.bind(S3))(
       // eslint-disable-next-line
