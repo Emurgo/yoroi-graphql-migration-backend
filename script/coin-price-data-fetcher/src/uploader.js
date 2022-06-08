62da1c8b133119c11803cf5973f9057a7bb796fe
yushi <yushihuang@gmail.com>
2022-06-08 10:18:48 +0800

    lazily initialize S3

--- script/coin-price-data-fetcher/src/uploader.js
+++ script/coin-price-data-fetcher/src/uploader.js
@@ -6,16 +6,27 @@ const logger = require('./logger');
 
 import type { Ticker } from './types';
 
-AWS.config.update({region: config.get("s3.region")});
+let _S3 = null;
 
-const S3 = new AWS.S3({
-  accessKeyId: config.get('s3.accessKeyId'),
-  secretAccessKey: config.get('s3.secretAccessKey'),
-});
+function getS3() {
+  if (_S3) {
+    return _S3;
+  }
+
+  AWS.config.update({region: config.get("s3.region")});
+
+  _S3 = new AWS.S3({
+    accessKeyId: config.get('s3.accessKeyId'),
+    secretAccessKey: config.get('s3.secretAccessKey'),
+  });
+
+  return _S3;
+}
 
 const RETRY_COUNT = 3;
 
 async function upload(ticker: Ticker): Promise<void> {
+  const S3 = getS3();
   const fileName = `prices-${ticker.from}-${ticker.timestamp}.json`;
   const uploadParams = {
     Body: JSON.stringify(ticker),
