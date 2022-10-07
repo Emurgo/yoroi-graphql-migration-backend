29f1e54554f636b4f41bf697ea844e91f9d43a15
Pedro <pedromtcosta@users.noreply.github.com>
2022-10-07 11:45:28 -0300

    WIP: configure Neo4j and skip creating Postgres views

--- src/v3/txs/index.ts
+++ src/v3/txs/index.ts
@@ -1,5 +1,12 @@
+import neo4j from "neo4j-driver";
 import { history } from "./history";
+import config from "config";
+
+const driver = neo4j.driver(
+  config.get("neo4j.url"),
+  neo4j.auth.basic(config.get("neo4j.username"), config.get("neo4j.password"))
+);
 
 export const txs = {
-  history
+  history: history(driver)
 };
