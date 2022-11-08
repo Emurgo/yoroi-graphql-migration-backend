0464ea7507a7fc062583da85c0fea54d70fed7b3
Pedro <pedromtcosta@users.noreply.github.com>
2022-11-08 14:30:22 -0300

    add v3 validate NFT

--- src/v3/multiAsset/index.ts
+++ src/v3/multiAsset/index.ts
@@ -1,8 +1,10 @@
 import { Driver } from "neo4j-driver";
 import { metadata } from "./metadata";
 import { policyIdExists } from "./policyIdExists";
+import { validateNFT } from "./validateNFT";
 
 export const multiAsset = (driver: Driver) => ({
   metadata: metadata(driver),
-  policyIdExists: policyIdExists(driver)
+  policyIdExists: policyIdExists(driver),
+  validateNFT: validateNFT(driver)
 });
