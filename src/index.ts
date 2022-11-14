9da5d22e595e0a906eb337aa33077a8f89087557
Andy Mars <7377166+DoctorMarshall@users.noreply.github.com>
2022-11-14 11:59:32 +0100

    migrated mintTxs endpoint

--- src/index.ts
+++ src/index.ts
@@ -772,6 +772,11 @@ const routes: Route[] = [
     path: "/v3/pool/info",
     method: "post",
     handler: v3.pool.info.handler
+  },
+  {
+    path: "/v3/asset/:fingerprint/mintTxs",
+    method: "get",
+    handler: v3.asset.mintTxs.handler
   }
 ];
 
