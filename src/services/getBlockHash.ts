a3c0fac3e9e1592dc554738728ee2bffeb23e6ae
Ahmed Ibrahim <ib153507@gmail.com>
2022-11-25 10:52:09 +0200

    Execute queries in parallel and return the response

--- src/services/getBlockHash.ts
+++ src/services/getBlockHash.ts
@@ -2,35 +2,51 @@ import { Request, Response } from "express";
 import { Pool } from "pg";
 
 const query = `
-  SELECT hash, epoch_no, slot_no FROM block WHERE (epoch_no, slot_no) IN ( ( 1, 23761) );
+    SELECT encode(hash, 'hex') as hash
+    FROM   block
+    WHERE  epoch_no = $1 AND slot_no <= $2
+    ORDER  BY slot_no DESC
+    LIMIT  1; 
 `;
 
-type BlockHash = [number, number];
+type EpochNo = number;
+type SlotNo = number;
+type Slot = [EpochNo, SlotNo];
+const MAX_SLOTS = 50;
+
 export const hanldeGetBlockHashBySlot =
   (pool: Pool) =>
-  async (req: Request<any, any, { slots: Array<[number , number]>}>, res: Response): Promise<void> => {
+  async (
+    req: Request<any, any, { slots: Array<Slot> }>,
+    res: Response
+  ): Promise<void> => {
     const { slots } = req.body;
 
-    let err: string | null  = null;
+    let err: string | null = null;
     if (!slots) err = "Missing 'slots' in the request body";
-    else if (!( slots instanceof Array )) err = "'slots' is required to be an array";
+    else if (!(slots instanceof Array)) err = "'slots' is required to be an array";
     else if (slots.length === 0) err = "'slots' is required to be non-empty";
-    else if (slots.length > 50) err = "The maximum number of slots allowed in the request is 50";
-    else if (slots.some(s => !( s instanceof Array) || s.length != 2)) err = "Each slot entry should be a tuple of two numbers: epoch and slot.";
+    else if (slots.length > MAX_SLOTS) err = "The maximum number of slots allowed is " + MAX_SLOTS;
+    else if (slots.some((s) => !(s instanceof Array) || s.length != 2))
+      err = "Each slot entry should be a tuple of two numbers: epoch and slot.";
     if (err !== null) throw new Error(err);
 
-    // if (
-    //     !(slots instanceof Array) || 
-    //     slots.length === 0 || 
-    //     slots.length > 50 || 
-    //     slots.some(s => !(s instanceof Array) || s.length != 2) 
-    // )
-    //     throw new Error(`The field 'slots' is required and is required to be a non-empty array. Each entry is a tuple of two numbers: epoch and slot.`);
+    const blockHashes: { [key: string]: string | null } = {};
 
+    const result = await Promise.all(
+      slots.map((slot) =>
+        pool
+          .query(query, [slot[0], slot[1]])
+          .then((res) => ({ rows: res.rows, slot }))
+      )
+    );
 
-    
-    const result = await pool.query("SELECT hash, epoch_no, slot_no FROM block LIMIT 20;", [
-        // [1, 2, 3, 4, 5]
-    ]);
-    res.status(200).json(result.rows);
+    result.forEach((res) => {
+      blockHashes[res.slot as any] =
+        res.rows.length != 0 ? res.rows[0].hash : null;
+    });
+
+    res.status(200).json({
+      blockHashes,
+    });
   };
