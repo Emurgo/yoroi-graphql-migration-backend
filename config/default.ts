629c8fed02b9ed50dfad3d02ab69c624788eaca3
Ahmed Ibrahim <ib153507@gmail.com>
2022-12-19 16:26:42 +0200

    Add dotenv to the config file

--- config/default.ts
+++ config/default.ts
@@ -1,3 +1,7 @@
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./.env" });
+
 export default { 
   db: {
     user: process.env.POSTGRES_USER || "hasura",
@@ -5,7 +9,7 @@ export default {
     database: process.env.POSTGRES_DB || "cexplorer",
     password: process.env.POSTGRES_PASSWORD || "",
     port: process.env.POSTGRES_PORT || 5432
- },
+  },
   server: {
     addressRequestLimit: 500,
     apiResponseLimit: 50,
