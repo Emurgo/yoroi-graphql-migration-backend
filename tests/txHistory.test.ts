a655fd4083cc6f006e261b8fb55b85e2704fa95c
Ahmed Ibrahim <ib153507@gmail.com>
2023-02-01 14:37:06 +0200

    Add tests & update docs for the tx history endpoint

--- tests/txHistory.test.ts
+++ tests/txHistory.test.ts
@@ -11,6 +11,13 @@ const { encode, toWords } = bech32;
 
 const endpoint = config.apiUrl;
 
+function failIfNotAxiosError(err: unknown) {
+ if (!axios.isAxiosError(err)) {
+  console.log(err);
+  throw err;
+ }
+}
+
 // these tests were written against http://10hk-mainnet.yoroiwallet.com/api/v2/
 // when they passed there, I pointed them against this repo and made that repo
 // pass as well.
@@ -100,44 +107,44 @@ const dataTxOrdering = {
 
 const dataShelleyCerts = {
   addresses: [
-     "addr1q9ya8v4pe33nlkgftyd70nhhp407pvnjjcsddhf64sh9gegwtvyxm7r69gx9cwvtg82p87zpwmzj0kj7tjmyraze3pzqe6zxzv"
-      // enterprise address
-    ,"addr1v8vqle5aa50ljr6pu5ndqve29luch29qmpwwhz2pk5tcggqn3q8mu"
-  ]
-  , untilBlock: "d6f6cd7101ce4fa80f7d7fe78745d2ca404705f58247320bc2cef975e7574939"
+    "addr1q9ya8v4pe33nlkgftyd70nhhp407pvnjjcsddhf64sh9gegwtvyxm7r69gx9cwvtg82p87zpwmzj0kj7tjmyraze3pzqe6zxzv",
+    // enterprise address
+    "addr1v8vqle5aa50ljr6pu5ndqve29luch29qmpwwhz2pk5tcggqn3q8mu"
+  ],
+  untilBlock: "d6f6cd7101ce4fa80f7d7fe78745d2ca404705f58247320bc2cef975e7574939"
 };
 
 const certificateRegistrationRewardAddresses = {
   addresses: [
     "e19842145a1693dfbf809963c7a605b463dce5ca6b66820341a443501e"
-  ]
-  , after: {
+  ],
+  after: {
     tx: "a235a7bb9b92cd04bd3c445f5659c2f1d55b3c71b51a0b2718a06ffe56095c66",
     block: "55ee284def9e92a08246752a9e6c2d4143b94e86dc32b06eeb4db042a415a6f9",
-  }
-  , untilBlock: "7e75460322424b2fa77b4550090977c51d3342668ad688186c2443f292c2932b"
+  },
+  untilBlock: "7e75460322424b2fa77b4550090977c51d3342668ad688186c2443f292c2932b"
 };
 const certificateDelegationRewardAddresses = {
   addresses: [
     "e1c3892366f174a76af9252f78368f5747d3155ab3568ea3b6bf40b01e", // fake address
     "e1c3892366f174a76af9252f78368f5747d3055ab3568ea3b6bf40b01e"
-  ]
-  , after: {
+  ],
+  after: {
     tx: "717ee5005ec1888b2b67195047d5d5d08a278fa52201d33686010c834cde24bd",
     block: "094ae9802b7e0a8cee97e88cc14a3029f8788d9cb9568ae32337e6ba2c0c1a5b",
-  }
-  , untilBlock: "fa0a6a6c3bea2a9dfae92ca2dfe89ec608ccf22f259e26d1e510503d74e7d3a0"
+  },
+  untilBlock: "fa0a6a6c3bea2a9dfae92ca2dfe89ec608ccf22f259e26d1e510503d74e7d3a0"
 };
 
 const certificateDeregistrationRewardAddresses = {
   addresses: [
     "e1aa377459e2cc1f7d81752f5a13e0eb1a4f85deebe8bb14bb1e157487"
-  ]
-  , after: {
+  ],
+  after: {
     tx: "9f93abce0b293b01f62ce9c8b0257a3da8aef27de164a609c32c92dc0a04f58e",
     block: "3d85a2fca53596e3b91d031d1d675b64c3b85db235ead56e04e951debd1833ec",
-  }
-  , untilBlock: "8fe8cafa28015c57225dda10d780c61479679124674f3db6ae6572b38b8feee1"
+  },
+  untilBlock: "8fe8cafa28015c57225dda10d780c61479679124674f3db6ae6572b38b8feee1"
 };
 
 
@@ -166,8 +173,11 @@ describe("/txs/history", function() {
       });
       expect(1).to.be.equal(0); // equivalent to asset false
     } catch (err) {
-      expect(err.response.status).to.be.equal(500);
-      expect(err.response.data.error.response).to.be.equal("REFERENCE_BEST_BLOCK_MISMATCH");
+      failIfNotAxiosError(err);
+      if (axios.isAxiosError(err)) {
+        expect(err.response?.status).to.be.equal(500);
+        expect(err.response?.data.error.response).to.be.equal("REFERENCE_BEST_BLOCK_MISMATCH");
+      }
     }
   });
   it("should throw reference errors for a tx that doesn't exist.", async() => {
@@ -182,8 +192,11 @@ describe("/txs/history", function() {
       });
       expect(1).to.be.equal(0); // equivalent to asset false
     } catch (err) {
-      expect(err.response.status).to.be.equal(500);
-      expect(err.response.data.error.response).to.be.equal("REFERENCE_TX_NOT_FOUND");
+      failIfNotAxiosError(err);
+      if (axios.isAxiosError(err)) {
+        expect(err.response?.status).to.be.equal(500);
+        expect(err.response?.data.error.response).to.be.equal("REFERENCE_TX_NOT_FOUND");
+      }
     }
   });
   it("should throw reference errors for a tx that doesn't match the block in after.", async() => {
@@ -198,8 +211,11 @@ describe("/txs/history", function() {
       });
       expect(1).to.be.equal(0); // equivalent to asset false
     } catch (err) {
-      expect(err.response.status).to.be.equal(500);
-      expect(err.response.data.error.response).to.be.equal("REFERENCE_BLOCK_MISMATCH");
+      failIfNotAxiosError(err);
+      if (axios.isAxiosError(err)) {
+        expect(err.response?.status).to.be.equal(500);
+        expect(err.response?.data.error.response).to.be.equal("REFERENCE_BLOCK_MISMATCH");
+      }
     }
   });
 
@@ -244,15 +260,15 @@ describe("/txs/history", function() {
     const result = await axios.post(testableUri, {
       addresses: [
         "addr1q84shx6jr9s258r9m45ujeyde7u4z7tthkedezjm5kdr4um64gv6jqqncjd205c540fgu5450tzvu27n9lk8ulm3s99spva2ru"
-      ]
+      ],
       // make sure if we as for after txIndex 0, txIndex 1 is included in the response
       // AKA support pagination mid-block
-      , after: {
+      after: {
         tx: "f07d7d5cb0126da7da9f6a067aee00fd42efae94891a42544abfd1759248019d",
         block: "728ceadf2d949281591175a6d1641f10f2307eff80eaf59c5300dbd4a5f83554",
-      }
+      },
       // make sure untilBlock is inclusive
-      , untilBlock: "728ceadf2d949281591175a6d1641f10f2307eff80eaf59c5300dbd4a5f83554"
+      untilBlock: "728ceadf2d949281591175a6d1641f10f2307eff80eaf59c5300dbd4a5f83554"
     });
     expect(result.data).to.have.lengthOf(1);
     expect(result.data[0].hash).to.equal("00d6d64b251514c48a9ad75940c5e7031bae5f0d002e9be7f6caf4cc1a78b57f");
@@ -263,12 +279,12 @@ describe("/txs/history", function() {
       const result = await axios.post(testableUri, {
         addresses: [
           "Ae2tdPwUPEZLs4HtbuNey7tK4hTKrwNwYtGqp7bDfCy2WdR3P6735W5Yfpe"
-        ]
-        , after: {
+        ],
+        after: {
           tx: "aef8aa952a11b1225f1c067824f38e0c4b6d478900db6b57f6503b81fbc09427",
           block: "07d8aee8a94c6a65b0a6dac7bb43e7f8ddb7320d3c7770db8b1be4fbd685c0aa",
-        }
-        , untilBlock: "187c5137b0c2660ad8277c843ddec0deede6da5c2ba50ac8b958127c328ddbee"
+        },
+        untilBlock: "187c5137b0c2660ad8277c843ddec0deede6da5c2ba50ac8b958127c328ddbee"
       });
       expect(result.data).to.have.lengthOf(1);
       expect(result.data[0].hash).to.equal("130f9c6f3dcb0af0733757b301c877ec63d5c127373e75268e8b20c09fa645df");
@@ -279,24 +295,27 @@ describe("/txs/history", function() {
       const result = await axios.post(testableUri, {
         addresses: [
           "addr1q9ya8v4pe33nlkgftyd70nhhp407pvnjjcsddhf64sh9gegwtvyxm7r69gx9cwvtg82p87zpwmzj0kj7tjmyraze3pzqe6zxzv"
-        ]
-        , untilBlock: "e99b06115fc0cd221671b686b6d9ef1c6dc047abed2c4f7d3ae528427a746f60"
+        ],
+        untilBlock: "e99b06115fc0cd221671b686b6d9ef1c6dc047abed2c4f7d3ae528427a746f60"
       });
       expect(result.data).to.have.lengthOf(1);
       expect(result.data[0].hash).to.equal("871b14fbe5abb6cacc63f922187c4f10ea9499055a972eb5d3d4e8771af643df");
       expect(result.data[0].type).to.equal("shelley");
     }
   });
+
   it("untilBlock should limit the response", async() => {
     const data = R.merge(dataForAddresses, { untilBlock: hashForOlderBlock } );
     const result = await axios.post(testableUri, data);
     const last = result.data[result.data.length - 1];
     expect(Date.parse(last.time)).to.be.at.most(timeForOlderBlock);
   });
+  
   it("single history objects should match iohk-mainnet", async () => {
     const result = await axios.post(testableUri, dataSingleHistory );
     expect(result.data).to.be.eql(resultsForSingleHistory);
   });
+
   it("objects should have all the properties", async() => {
     const result = await axios.post(testableUri, dataSingleHistory );
     const obj = result.data[0];
@@ -324,8 +343,8 @@ describe("/txs/history", function() {
     expect(obj.inputs[0]).to.have.property("txHash");
     expect(obj.outputs[0]).to.have.property("address");
     expect(obj.outputs[0]).to.have.property("amount");
-
   });
+
   it("order of tx objects should be by block_num asc, tx_ordinal asc", async() => {
     const result = await axios.post(testableUri, dataTxOrdering);
     const sortedList = R.sortBy((obj: any) => obj.block_num, result.data);
@@ -339,6 +358,7 @@ describe("/txs/history", function() {
     }
     
   });
+
   it("order for tx output objects should be by tx_index (aka tx_ordinal)", async() => {
     const result = await axios.post(testableUri, dataTxOrdering);
     // the order index is not actually available, so we just check that they values we get back are the ones we want.
@@ -349,28 +369,32 @@ describe("/txs/history", function() {
     expect(result.data[0].outputs[1].address).to.be.eql("Ae2tdPwUPEYynjShTL8D2L2GGggTH3AGtMteb7r65oLar1vzZ4JPfxob4b8");
     expect(result.data[0].outputs[1].amount).to.be.eql("98000000");
   });
+
   it("Get payment key that only occurs in input", async() => {
     const result = await axios.post(testableUri, {
       addresses: [
         encode(Prefixes.PAYMENT_KEY_HASH, toWords(Buffer.from("211c082781577c6b8a4832d29011baab323947e59fbd6ec8995b6c5a", "hex")))
-      ]
-      , after: {
+      ],
+      after: {
         block: "b51b1605cc27b0be3a1ab07dfcc2ceb0b0da5e8ab5d0cb944c16366edba92e83",
         tx: "79acf08126546b68d0464417af9530473b8c56c63b2a937bf6451e96e55cb96a",
-      }, untilBlock: "f0d4b1eed671770194a223eaba3fc0cb0b6787d83c432ec5c24b83620c9b7474"
+      },
+      untilBlock: "f0d4b1eed671770194a223eaba3fc0cb0b6787d83c432ec5c24b83620c9b7474"
     });
     expect(result.data).to.have.lengthOf(1);
     expect(result.data[0].hash).to.equal("92bdc4f35fd9b363a4eac47898148fd1816efd4260d71e8251ca80dbb7a39ca3");
   });
+
   it("Get payment key that only occurs in output", async() => {
     const result = await axios.post(testableUri, {
       addresses: [
         encode(Prefixes.PAYMENT_KEY_HASH, toWords(Buffer.from("85abf3eca55024aa1c22b944599b5e890ec12dfb19941229da4ba293", "hex")))
-      ]
-      , untilBlock: "094ae9802b7e0a8cee97e88cc14a3029f8788d9cb9568ae32337e6ba2c0c1a5b"
+      ],
+      untilBlock: "094ae9802b7e0a8cee97e88cc14a3029f8788d9cb9568ae32337e6ba2c0c1a5b"
     });
     expect(result.data).to.have.lengthOf(1);
   });
+
   it("should get sensible shelley certificates", async() => {
     const result = await axios.post(testableUri, dataShelleyCerts);
     const resultsWithCerts = result.data.filter( (obj: TransactionFrag) => obj.certificates.length > 0);
@@ -398,12 +422,13 @@ describe("/txs/history", function() {
       expect(typeof mirCert.rewards[addr]).to.be.equal("string");
     }
   });
+
   it("(deprecated) should respond to reward addresses with relevant witnesses", async() => {
     const result = await axios.post(testableUri, {
       addresses: [
         "e19842145a1693dfbf809963c7a605b463dce5ca6b66820341a443501e"
-      ]
-      , untilBlock: "d6f6cd7101ce4fa80f7d7fe78745d2ca404705f58247320bc2cef975e7574939"
+      ],
+      untilBlock: "d6f6cd7101ce4fa80f7d7fe78745d2ca404705f58247320bc2cef975e7574939"
     });
     expect(result.data).to.not.be.empty;
 
@@ -413,12 +438,13 @@ describe("/txs/history", function() {
       result.data.map( (obj: TransactionFrag) => obj.hash)
     );
   });
+
   it("should respond to reward addresses with relevant witnesses", async() => {
     const result = await axios.post(testableUri, {
       addresses: [
         "stake1uxvyy9z6z6fal0uqn93u0fs9k33aeew2ddngyq6p53p4q8smzq4sz"
-      ]
-      , untilBlock: "d6f6cd7101ce4fa80f7d7fe78745d2ca404705f58247320bc2cef975e7574939"
+      ],
+      untilBlock: "d6f6cd7101ce4fa80f7d7fe78745d2ca404705f58247320bc2cef975e7574939"
     });
     expect(result.data).to.not.be.empty;
 
@@ -428,6 +454,7 @@ describe("/txs/history", function() {
       result.data.map( (obj: TransactionFrag) => obj.hash)
     );
   });
+
   it("should respond to reward addresses with relevant certificates", async() => {
     // registration cert
     {
@@ -462,12 +489,12 @@ describe("/txs/history", function() {
       const result = await axios.post(testableUri, {
         addresses: [
           "e17ee7b9a540f56912c3e6937f1ef819c8862c33efe80ab1605105aeae"
-        ]
-        , after: {
+        ],
+        after: {
           tx: "b5a02c29a0ea54e1b371d63cd2997c922398d66a733a6701e7cb5aa6f59fbebb",
           block: "22f010b4aa18ad964dae9feac5407cbe453110ebd85fc977c9ffdb7a193a1378",
-        }
-        , untilBlock: "f094414de98d9c7e80ed8c29b14f5866bcbb648c2aee8e8869df89c23c45fe25"
+        },
+        untilBlock: "f094414de98d9c7e80ed8c29b14f5866bcbb648c2aee8e8869df89c23c45fe25"
       });
       expect(result.data).to.not.be.empty;
 
@@ -476,16 +503,17 @@ describe("/txs/history", function() {
       expect(result.data[0].certificates[0].kind).to.equal("PoolRegistration");
     }
   });
+
   it("Get metadata for transaction", async() => {
     // tx that only has a single metadata entry
     const singleMetadataTx = await axios.post(testableUri, {
       addresses: [
         encode(Prefixes.PAYMENT_KEY_HASH, toWords(Buffer.from("69f6f57453031d04bd71a08cd3f31e7d61bfa939037f0e547de850e3", "hex")))
-      ]
-      , untilBlock: "d9038b728b997566b4b5fd2686ed2268505f50c8faa1292837fede6ef42cdab5"
-      , after: {
-          tx: "0e06078128b5126c77cf6ffe68eab7d0d51423cba84f24d34c433752ff0c843b"
-        , block: "7b483248865efe366af230a68952340ec2a5868433a3323abfff433699997175"
+      ],
+      untilBlock: "d9038b728b997566b4b5fd2686ed2268505f50c8faa1292837fede6ef42cdab5",
+      after: {
+          tx: "0e06078128b5126c77cf6ffe68eab7d0d51423cba84f24d34c433752ff0c843b",
+          block: "7b483248865efe366af230a68952340ec2a5868433a3323abfff433699997175"
       }
     });
     expect(singleMetadataTx.data).to.have.lengthOf(1);
@@ -496,11 +524,11 @@ describe("/txs/history", function() {
     const multiMetadataTx = await axios.post(testableUri, {
       addresses: [
         encode(Prefixes.PAYMENT_KEY_HASH, toWords(Buffer.from("89aa7bae5ad34eb20ad28a9ade1ab539fbb8e957bb6645e47434a537", "hex")))
-      ]
-      , untilBlock: "159a22318dd76531fb34fa0ddf33198755a444b6452ddbabfb651ef2148294f8"
-      , after: {
-          tx: "e7d3a4fdbed209623fb858e206176511c30f7880dc89d3801d1b33cacdfc3e1c"
-        , block: "acf5ead409a27d4795f8a653a3ee6ba1550a7ea09331751a7aba3ba2fe3c47f0"
+      ],
+      untilBlock: "159a22318dd76531fb34fa0ddf33198755a444b6452ddbabfb651ef2148294f8",
+      after: {
+          tx: "e7d3a4fdbed209623fb858e206176511c30f7880dc89d3801d1b33cacdfc3e1c",
+          block: "acf5ead409a27d4795f8a653a3ee6ba1550a7ea09331751a7aba3ba2fe3c47f0"
       }
     });
     expect(multiMetadataTx.data).to.have.lengthOf(1);
@@ -512,13 +540,36 @@ describe("/txs/history", function() {
     const catalystRewardTx = await axios.post(testableUri, {
       addresses: [
         "stake1uypy44wqjznc5w9ns9gsguz4ta83jekrg9d0wupa7j3zsacwvq5ex"
-      ]
-      , untilBlock: "b687d2012739687a3543c1ec4b47b899b5519c94299cd6537174467707b979ad"
-      , after: {
-          tx: "0e06078128b5126c77cf6ffe68eab7d0d51423cba84f24d34c433752ff0c843b"
-        , block: "7b483248865efe366af230a68952340ec2a5868433a3323abfff433699997175"
+      ],
+      untilBlock: "b687d2012739687a3543c1ec4b47b899b5519c94299cd6537174467707b979ad",
+      after: {
+          tx: "0e06078128b5126c77cf6ffe68eab7d0d51423cba84f24d34c433752ff0c843b",
+          block: "7b483248865efe366af230a68952340ec2a5868433a3323abfff433699997175"
       }
     });
     expect(catalystRewardTx.data).to.have.lengthOf(2);
+  });
+
+  it("should query TXs from the next block if `after.tx` is missing", async() => {
+    const result = await axios.post(testableUri, {
+      addresses: ["Ae2tdPwUPEZHu3NZa6kCwet2msq4xrBXKHBDvogFKwMsF18Jca8JHLRBas7"],
+      untilBlock: hashForUntilBlock,
+      after: {
+        block: "790eb4d6ef2fea7cceebf22c66c20518616d5331966f6f9b4ca3a308b9c3ceb1"
+      },
+    });
+
+    const txHashes = [
+      "ea2b2abb9bf440a8ab89685e359e88399802c002383a21e4d09a7d636a9514d2",
+      "065467aa35cd4a53502941b7a9d58ba5f149e45f0f8bd6829a72f8de781ec6c3",
+      "e8acd016e530dce91b697ed702f9f8a82a11fc6c43c77074c223378e77152b01",
+      "c2e7ad11a93739883102dd9c4ff72143457603678c6c6d9b399128e5977c0829"
+    ];
+
+    expect(result.data).to.have.lengthOf(4);
+
+    txHashes.forEach((tx: string, idx: number) => {
+      expect(result.data[idx].hash).to.be.equal(tx);
+    });
   });
 });
