fe0df0b476ca7ed34fdfeb57b267e9dc7631b5e7
yushi <yushihuang@gmail.com>
2023-05-25 16:12:31 +0800

    remove the price fetcher

--- script/coin-price-data-fetcher/src/logger.js
+++
@@ -1,6 +0,0 @@
-//@flow
-const bunyan = require('bunyan');
-const config = require('config');
-const logger = bunyan.createLogger({name: 'price data fetcher', level: config.logger.level});
-
-module.exports = logger;
